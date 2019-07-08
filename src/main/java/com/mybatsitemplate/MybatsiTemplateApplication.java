ae189380eb837aacaff397b72127363ada07e0f6
周林 <zhoulin5>
2019-07-08 22:21:04 +0800

    MyBatis架构初始化

---
+++ src/main/java/com/mybatsitemplate/MybatsiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.mybatsitemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatsiTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatsiTemplateApplication.class, args);
+    }
+
+}
